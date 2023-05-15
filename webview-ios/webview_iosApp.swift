42df9fe32cbf368bffeea1b56173477a3844353d
zaeyon <wodus5677@gmail.com>
2023-05-15 15:20:16 +0900

    Initial Commit

---
+++ webview-ios/webview_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  webview_iosApp.swift
+//  webview-ios
+//
+//  Created by 이재연 on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct webview_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
